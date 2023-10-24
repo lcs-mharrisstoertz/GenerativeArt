1f90502de498b606025b85ceaf895f05c19c5727
Morgan Harris-Stoertz <mharrisstoertz@lcs.on.ca>
2023-10-24 11:44:41 -0400

    organized files in groups

---
+++ GenerativeArt/Views/MosaicViews.swift
@@ -0,0 +1,18 @@
+//
+//  MosaicViews.swift
+//  GenerativeArt
+//
+//  Created by Morgan Harris-Stoertz on 2023-10-24.
+//
+
+import SwiftUI
+
+struct MosaicViews: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MosaicViews()
+}
