ce34046fe49cd2fd7df29946b731cee770f348cc
Morgan Harris-Stoertz <mharrisstoertz@lcs.on.ca>
2023-10-28 10:21:23 -0400

    controlling rows and colums using stored properties

--- GenerativeArt/Views/MosaicViews.swift
+++ GenerativeArt/Views/MosaicViews.swift
@@ -8,20 +8,28 @@
 import SwiftUI
 
 struct MosaicViews: View {
+    //MARK: Stored properties
+    let columns = 7
+    let rows = 4
+    
+    //MARK: Computed properties
     var body: some View {
-        Grid(horizontalSpacing: 0, verticalSpacing: 0){
-            
-            //number of rows
-                ForEach(1..<5) { J in
-                    GridRow{
-                        
-                        //reapeats in a row (coloums)
-                        ForEach(1..<5) { i in
-                            TileView()
+        VStack {
+            Grid(horizontalSpacing: 0, verticalSpacing: 0){
+                
+                //number of rows
+                ForEach(0..<rows, id:\.self) { J in
+                        GridRow{
+                            
+                            //reapeats in a row (colums)
+                            ForEach(0..<columns, id: \.self) { i in
+                                TileView()
+                            }
                         }
                     }
-                }
-          
+              
+            }
+            
         }
     }
 }
