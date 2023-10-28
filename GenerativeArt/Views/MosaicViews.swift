84a61630320e06c42d53651b7d2f8ee1128e9a01
Morgan Harris-Stoertz <mharrisstoertz@lcs.on.ca>
2023-10-28 10:35:54 -0400

    moved the stored and computed property into the tile structure so each tile is different

--- GenerativeArt/Views/MosaicViews.swift
+++ GenerativeArt/Views/MosaicViews.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct MosaicViews: View {
     //MARK: Stored properties
     @State var columns = 7.0
-    @State var rows = 4
+    @State var rows = 4.0
     
     //MARK: Computed properties
     var body: some View {
@@ -18,7 +18,7 @@ struct MosaicViews: View {
             Grid(horizontalSpacing: 0, verticalSpacing: 0){
                 
                 //number of rows
-                ForEach(0..<rows, id:\.self) { J in
+                ForEach(0..<Int(rows), id:\.self) { J in
                     GridRow{
                         
                         //reapeats in a row (colums)
@@ -30,8 +30,11 @@ struct MosaicViews: View {
                 
             }
         }
-        
+        //number of coloumns
         Slider(value: $columns, in: 1...10, step: 1.0)
+        
+        //number of rows
+        Slider(value: $rows, in: 1...10, step: 1.0)
     }
 }
 
--- GenerativeArt/Views/TileView.swift
+++ GenerativeArt/Views/TileView.swift
@@ -7,47 +7,47 @@
 
 import SwiftUI
 
-
-//MARK: stored properties
-
-//determine diagonal direction
-let flipOne = Coin.flip()
-
-//determine the top and bottom colors
-let flipTwo = Coin.flip()
-
-//determine final fill color
-let flipThree = Coin.flip()
-
-
-//choose my markers
-let markerOne = Color.indigo
-let markerTwo = Color.mint
-
-//flip for color
-let flipForColor = Coin.flip()
-
-//MARK: Computed properties
-
-//set colors based on the coin flip
-var colorOne: Color{
-    return flipForColor == .heads ? markerOne : markerTwo
-}
-
-var colorTwo: Color{
-    return flipForColor == .heads ? markerTwo : markerOne
-    }
-
-//final flip for determining remaining fill color
-var remainingTriangleFillColor: Color{
-    if flipThree == . heads {
-        return colorTwo
-    } else {
-        return .clear
-    }
-}
-
 struct TileView: View {
+    
+    //MARK: stored properties
+
+    //determine diagonal direction
+    let flipOne = Coin.flip()
+
+    //determine the top and bottom colors
+    let flipTwo = Coin.flip()
+
+    //determine final fill color
+    let flipThree = Coin.flip()
+
+
+    //choose my markers
+    let markerOne = Color.indigo
+    let markerTwo = Color.mint
+
+    //flip for color
+    let flipForColor = Coin.flip()
+
+    //MARK: Computed properties
+
+    //set colors based on the coin flip
+    var colorOne: Color{
+        return flipForColor == .heads ? markerOne : markerTwo
+    }
+
+    var colorTwo: Color{
+        return flipForColor == .heads ? markerTwo : markerOne
+        }
+
+    //final flip for determining remaining fill color
+    var remainingTriangleFillColor: Color{
+        if flipThree == . heads {
+            return colorTwo
+        } else {
+            return .clear
+        }
+    }
+
     var body: some View {
         ZStack {
             if flipOne == .heads {
